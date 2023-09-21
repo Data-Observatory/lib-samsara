1b3a5ee4db3191a8efd684a407803cfac53d7d5d
valegui <valeriaguidotti@gmail.com>
2023-09-21 16:26:37 -0300

    docs: shaped kernel docstring

--- src/samsara/kernel/_basic.py
+++ src/samsara/kernel/_basic.py
@@ -16,6 +16,40 @@ __all__ = [
 
 
 def circle(radius: int, normalize: bool = False) -> Kernel:
+    """Circle shaped kernel.
+
+    Parameters
+    ----------
+    radius : int
+        The radius of the kernel to generate.
+    normalize : bool, optional
+        Normalize the kernel data values so its euclidean norm is equal to 1, by default False.
+
+    Returns
+    -------
+    Kernel
+        Kernel with data array shaped as a circle.
+
+    Examples
+    --------
+    >>> import samsara.kernel as skernel
+    >>> x = skernel.circle(3)
+    >>> x.data
+    array([[0., 0., 0., 1., 0., 0., 0.],
+           [0., 1., 1., 1., 1., 1., 0.],
+           [0., 1., 1., 1., 1., 1., 0.],
+           [1., 1., 1., 1., 1., 1., 1.],
+           [0., 1., 1., 1., 1., 1., 0.],
+           [0., 1., 1., 1., 1., 1., 0.],
+           [0., 0., 0., 1., 0., 0., 0.]])
+    >>> y = skernel.circle(2, True)
+    >>> y.data
+    array([[0.       , 0.       , 0.2773501, 0.       , 0.       ],
+           [0.       , 0.2773501, 0.2773501, 0.2773501, 0.       ],
+           [0.2773501, 0.2773501, 0.2773501, 0.2773501, 0.2773501],
+           [0.       , 0.2773501, 0.2773501, 0.2773501, 0.       ],
+           [0.       , 0.       , 0.2773501, 0.       , 0.       ]])
+    """
     side = 2 * radius + 1
     x, y = np.meshgrid(np.arange(side), np.arange(side))
     distance_from_center = np.sqrt((x - radius) ** 2 + (y - radius) ** 2)
@@ -28,6 +62,33 @@ def circle(radius: int, normalize: bool = False) -> Kernel:
 
 
 def cross(radius: int, normalize: bool = False) -> Kernel:
+    """Cross shaped kernel.
+
+    Parameters
+    ----------
+    radius : int
+        The radius of the kernel to generate.
+    normalize : bool, optional
+        Normalize the kernel data values so its euclidean norm is equal to 1, by default False.
+
+    Returns
+    -------
+    Kernel
+        Kernel with data array shaped as a cross.
+
+    Examples
+    --------
+    >>> import samsara.kernel as skernel
+    >>> x = skernel.cross(3)
+    >>> x.data
+    array([[0., 0., 0., 1., 0., 0., 0.],
+           [0., 0., 0., 1., 0., 0., 0.],
+           [0., 0., 0., 1., 0., 0., 0.],
+           [1., 1., 1., 1., 1., 1., 1.],
+           [0., 0., 0., 1., 0., 0., 0.],
+           [0., 0., 0., 1., 0., 0., 0.],
+           [0., 0., 0., 1., 0., 0., 0.]])
+    """
     side = 2 * radius + 1
     data = np.zeros((side, side))
     data[:, radius] = 1.0
@@ -40,6 +101,31 @@ def cross(radius: int, normalize: bool = False) -> Kernel:
 
 
 def custom(data: np.ndarray, normalize: bool = False) -> Kernel:
+    """Custom kernel.
+
+    Parameters
+    ----------
+    data : np.ndarray
+        The data array of the kernel to generate.
+    normalize : bool, optional
+        Normalize the kernel data values so its euclidean norm is equal to 1, by default False.
+
+    Returns
+    -------
+    Kernel
+        Kernel with a custom data array.
+
+    Examples
+    --------
+    >>> import samsara.kernel as skernel
+    >>> import numpy as np
+    >>> data = np.array([[1, 0, 1], [0, 1, 1], [0, 0, 0]])
+    >>> x = skernel.custom(data)
+    >>> x.data
+    array([[1, 0, 1],
+           [0, 1, 1],
+           [0, 0, 0]])
+    """
     if normalize:
         return Kernel(data=normalize_array(data), name="custom", normalized=True)
 
@@ -47,6 +133,33 @@ def custom(data: np.ndarray, normalize: bool = False) -> Kernel:
 
 
 def ex(radius: int, normalize: bool = False) -> Kernel:
+    """Ex (x) shaped kernel.
+
+    Parameters
+    ----------
+    radius : int
+        The radius of the kernel to generate.
+    normalize : bool, optional
+        Normalize the kernel data values so its euclidean norm is equal to 1, by default False.
+
+    Returns
+    -------
+    Kernel
+        Kernel with data array shaped as an ex (x).
+
+    Examples
+    --------
+    >>> import samsara.kernel as skernel
+    >>> x = skernel.ex(3)
+    >>> x.data
+    array([[1., 0., 0., 0., 0., 0., 1.],
+           [0., 1., 0., 0., 0., 1., 0.],
+           [0., 0., 1., 0., 1., 0., 0.],
+           [0., 0., 0., 1., 0., 0., 0.],
+           [0., 0., 1., 0., 1., 0., 0.],
+           [0., 1., 0., 0., 0., 1., 0.],
+           [1., 0., 0., 0., 0., 0., 1.]])
+    """
     side = 2 * radius + 1
     data = np.zeros((side, side))
     np.fill_diagonal(data, 1.0)
@@ -59,6 +172,33 @@ def ex(radius: int, normalize: bool = False) -> Kernel:
 
 
 def octagon(radius: int, normalize: bool = False) -> Kernel:
+    """Octagon shaped kernel.
+
+    Parameters
+    ----------
+    radius : int
+        The radius of the kernel to generate.
+    normalize : bool, optional
+        Normalize the kernel data values so its euclidean norm is equal to 1, by default False.
+
+    Returns
+    -------
+    Kernel
+        Kernel with data array shaped as an octagon.
+
+    Examples
+    --------
+    >>> import samsara.kernel as skernel
+    >>> x = skernel.octagon(3)
+    >>> x.data
+    array([[0., 0., 1., 1., 1., 0., 0.],
+           [0., 1., 1., 1., 1., 1., 0.],
+           [1., 1., 1., 1., 1., 1., 1.],
+           [1., 1., 1., 1., 1., 1., 1.],
+           [1., 1., 1., 1., 1., 1., 1.],
+           [0., 1., 1., 1., 1., 1., 0.],
+           [0., 0., 1., 1., 1., 0., 0.]])
+    """
     side = 2 * radius + 1
     x, y = np.meshgrid(
         np.abs(np.arange(side) - radius), np.abs(np.arange(side) - radius)
@@ -73,6 +213,33 @@ def octagon(radius: int, normalize: bool = False) -> Kernel:
 
 
 def rectangle(x_radius: int, y_radius: int, normalize: bool = False) -> Kernel:
+    """Rectangular shaped kernel.
+
+    Parameters
+    ----------
+    x_radius : int
+        The horizontal radius of the kernel to generate.
+    y_radius : int
+        The vertical radius of the kernel to generate.
+    normalize : bool, optional
+        Normalize the kernel data values so its euclidean norm is equal to 1, by default False.
+
+    Returns
+    -------
+    Kernel
+        Kernel with data array shaped as a rectangle.
+
+    Examples
+    --------
+    >>> import samsara.kernel as skernel
+    >>> x = skernel.rectangle(2, 3)
+    >>> x.data
+    array([[1., 1., 1., 1., 1., 1., 1.],
+           [1., 1., 1., 1., 1., 1., 1.],
+           [1., 1., 1., 1., 1., 1., 1.],
+           [1., 1., 1., 1., 1., 1., 1.],
+           [1., 1., 1., 1., 1., 1., 1.]])
+    """
     x_side = 2 * x_radius + 1
     y_side = 2 * y_radius + 1
     data = np.ones((x_side, y_side))
@@ -84,6 +251,33 @@ def rectangle(x_radius: int, y_radius: int, normalize: bool = False) -> Kernel:
 
 
 def rhombus(radius: int, normalize: bool = False) -> Kernel:
+    """Rhombus (diamond) shaped kernel.
+
+    Parameters
+    ----------
+    radius : int
+        The radius of the kernel to generate.
+    normalize : bool, optional
+        Normalize the kernel data values so its euclidean norm is equal to 1, by default False.
+
+    Returns
+    -------
+    Kernel
+        Kernel with data array shaped as a rhombus (diamond).
+
+    Examples
+    --------
+    >>> import samsara.kernel as skernel
+    >>> x = skernel.rhombus(3)
+    >>> x.data
+    array([[0., 0., 0., 1., 0., 0., 0.],
+           [0., 0., 1., 1., 1., 0., 0.],
+           [0., 1., 1., 1., 1., 1., 0.],
+           [1., 1., 1., 1., 1., 1., 1.],
+           [0., 1., 1., 1., 1., 1., 0.],
+           [0., 0., 1., 1., 1., 0., 0.],
+           [0., 0., 0., 1., 0., 0., 0.]])
+    """
     side = 2 * radius + 1
     x, y = np.meshgrid(
         np.abs(np.arange(side) - radius), np.abs(np.arange(side) - radius)
@@ -97,6 +291,33 @@ def rhombus(radius: int, normalize: bool = False) -> Kernel:
 
 
 def square(radius: int, normalize: bool = False) -> Kernel:
+    """Square shaped kernel.
+
+    Parameters
+    ----------
+    radius : int
+        The radius of the kernel to generate.
+    normalize : bool, optional
+        Normalize the kernel data values so its euclidean norm is equal to 1, by default False.
+
+    Returns
+    -------
+    Kernel
+        Kernel with data array shaped as a square.
+
+    Examples
+    --------
+    >>> import samsara.kernel as skernel
+    >>> x = skernel.square(3)
+    >>> x.data
+    array([[1., 1., 1., 1., 1., 1., 1.],
+           [1., 1., 1., 1., 1., 1., 1.],
+           [1., 1., 1., 1., 1., 1., 1.],
+           [1., 1., 1., 1., 1., 1., 1.],
+           [1., 1., 1., 1., 1., 1., 1.],
+           [1., 1., 1., 1., 1., 1., 1.],
+           [1., 1., 1., 1., 1., 1., 1.]])
+    """
     side = 2 * radius + 1
     data = np.ones((side, side))
 
