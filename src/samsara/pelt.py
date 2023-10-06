1d845423032fb43d73cebca3fad19c13957674ff
valegui <valeriaguidotti@gmail.com>
2023-10-06 12:51:57 -0300

    feat: pelt and block pelt functions

--- src/samsara/pelt.py
+++ src/samsara/pelt.py
@@ -1,7 +1,126 @@
+import datetime
 from typing import Union
 
+import dask.array as da
 import numpy as np
 import ruptures as rpt
+import xarray as xr
+from numba import float32, float64, guvectorize
+
+# For the whole image in the whole time series
+
+
+def pelt(
+    array: xr.DataArray,
+    n_breaks: int = 5,  #
+    penalty: float = 30,
+    start_date: Union[str, None] = None,
+    **kwargs,
+) -> tuple(da.Array, da.Array):
+    data = array.data
+    dates = array.time.values
+    chunks = ((n_breaks, n_breaks), data.chunks[1], data.chunks[2])
+    # Each chunk, that contains the whole time series, will generate 2 chunks, where the first is
+    # the mean magnitude and the second is the dates. There is no problem with iterated magnitude
+    # values and dates because the time dimension is not chunked.
+
+    break_cubes = da.map_blocks(
+        block_pelt,
+        data,
+        dates,
+        n_breaks,
+        penalty,
+        start_date,
+        **kwargs,
+        dtype=float,
+        chunks=chunks,
+        drop_axis=0,
+        new_axis=0,
+    )
+
+    magnitude_cube = break_cubes[:n_breaks]
+    dates_cube = break_cubes[n_breaks:]
+
+    return magnitude_cube, dates_cube
+
+
+def block_pelt(
+    array: np.ndarray,
+    dates: np.ndarray,
+    n_breaks: int,
+    penalty: float,
+    start_date: Union[str, None],
+    kernel_model: str = "rbf",
+) -> np.ndarray:
+    working_idx = None
+    if start_date is None:
+        working_idx = filter_index_by_date(dates, start_date)  # 1d array
+
+    break_idx = block_breakpoints_index(
+        array, penalty, n_breaks, model=kernel_model, valid_index=working_idx
+    )  # 3d array (n_breaks, y, x)
+
+    segment_mean_mag, segment_dates = block_segment_metrics(array, dates, break_idx)
+
+    return np.vstack(segment_mean_mag, segment_dates)
+
+
+def filter_index_by_date(dates: np.ndarray, start_date: str):
+    start_date_np = np.datetime64(start_date)
+    indices = np.argwhere(dates > start_date_np).ravel()
+    # return np.take(array, indices, axis=0), indices
+    return indices
+
+
+def block_breakpoints_index(
+    array: np.ndarray,
+    penalty: float,
+    n_breaks: int,
+    model: str,
+    valid_index: Union[np.ndarray, None],
+) -> np.ndarray:
+    # Non-jagged output
+    algo = rpt.KernelCPD(kernel=model, min_size=3, jump=5)
+
+    def predict_unique_index(array_1d, valid_index):
+        breaks = np.full((n_breaks), np.nan)
+        breaks_ = algo.fit(array_1d).predict(pen=penalty)
+        breaks_ = np.array(breaks_[:-1]) - 1
+        # Get valid breakpoint indices
+        if valid_index is not None:
+            break_idx = np.intersect1d(breaks_, valid_index)
+        else:
+            break_idx = np.unique(breaks_)
+        # Lowest index that meets the number of expected breaks
+        n_breaks_ = min(n_breaks, len(break_idx))
+        breaks[:n_breaks_] = break_idx[:n_breaks_]
+        return breaks
+
+    breaks = np.apply_along_axis(predict_unique_index, 0, array, valid_index)
+    return breaks
+
+
+def block_segment_metrics(array: np.ndarray, dates: np.ndarray, break_idx: np.ndarray):
+    return
+
+
+@guvectorize(
+    [(float32[:], float32[:], float32[:]), (float64[:], float64[:], float64[:])],
+    "(),()->()",
+)
+def segment_mean(array: np.ndarray, break_idx: np.ndarray):
+    return
+
+
+@guvectorize(
+    [(float32[:], float32[:], float32[:]), (float64[:], float64[:], float64[:])],
+    "(),()->()",
+)
+def segment_dates(dates: np.ndarray, break_idx: np.ndarray):
+    return
+
+
+# For 1 pixel
 
 
 def breakpoints(
@@ -42,13 +161,6 @@ def breakpoints_index(array: np.ndarray, penalty: float, model: str = "rbf") -> 
     return breaks
 
 
-def filter_index_by_date(dates: np.ndarray, start_date: str):
-    start_date_np = np.datetime64(start_date)
-    indices = np.argwhere(dates > start_date_np).ravel()
-    # return np.take(array, indices, axis=0), indices
-    return indices
-
-
 def segment_metrics(array: np.ndarray, dates: np.ndarray, break_idx: np.ndarray):
     break_idx = break_idx.tolist()
     if break_idx[0] != 0:
@@ -70,8 +182,6 @@ def segment_metrics(array: np.ndarray, dates: np.ndarray, break_idx: np.ndarray)
 
 
 def _year_fraction(date):
-    import datetime
-
     year = date.astype("datetime64[Y]").astype(int) + 1970
     month = date.astype("datetime64[M]").astype(int) % 12 + 1
     day = (date - date.astype("datetime64[M]") + 1).astype(int)
