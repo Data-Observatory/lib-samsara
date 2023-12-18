ecf1e88d5c3869482563f8fd012889c613f5deee
valegui <valeriaguidotti@gmail.com>
2023-12-18 14:20:47 -0300

    refactor: move feature to glcm subpackage

--- src/samsara/stats/_features.py
+++ src/samsara/stats/_features.py
@@ -2,7 +2,7 @@ from typing import Union
 
 import numpy as np
 
-__all__ = ["features"]
+__all__ = ["correlation", "entropy", "plus_minus"]
 
 
 def plus_minus(p: np.ndarray, px_plus_y: np.ndarray, px_minus_y: np.ndarray) -> tuple:
@@ -36,53 +36,3 @@ def correlation(
     if sx == 0.0 or sy == 0.0:
         return 1.0
     return (1.0 / sx / sy) * (np.dot(ij.ravel(), pravel) - ux * uy)
-
-
-def features(array: np.ndarray, n_feats: int = 7) -> np.ndarray:
-    fts = np.full((n_feats,), np.nan)
-
-    maxv = len(array)
-    k = np.arange(maxv)
-    k2 = k**2
-    tk = np.arange(2 * maxv)
-    # tk2 = tk**2
-    i, j = np.mgrid[:maxv, :maxv]
-    ij = i * j
-    i_j2_p1 = (i - j) ** 2
-    i_j2_p1 += 1
-    i_j2_p1 = 1.0 / i_j2_p1
-    i_j2_p1 = i_j2_p1.ravel()
-
-    p = array / float(array.sum())
-    pravel = p.ravel()
-    px = p.sum(0)
-    py = p.sum(1)
-
-    ux = np.dot(px, k)
-    uy = np.dot(py, k)
-    vx = np.dot(px, k2) - ux**2
-    vy = np.dot(py, k2) - uy**2
-
-    sx = np.sqrt(vx)
-    sy = np.sqrt(vy)
-
-    px_plus_y = np.full(2 * maxv, fill_value=0, dtype=np.double)
-    px_minus_y = np.full(maxv, fill_value=0, dtype=np.double)
-    px_plus_y, px_minus_y = plus_minus(p, px_plus_y, px_minus_y)
-
-    fts = [
-        np.dot(pravel, pravel),  # 1. ASM
-        np.dot(k2, px_minus_y),  # 2. Contrast
-        correlation(sx, sy, ux, uy, pravel, ij),  # 3. Correlation
-        vx,  # 4. Variance
-        np.dot(i_j2_p1, pravel),  # 5. Inverse Difference Moment
-        np.dot(tk, px_plus_y),  # 6. Sum Average
-        entropy(pravel),  # 9. Entropy
-    ]
-
-    if len(fts) < n_feats:
-        return np.pad(
-            np.array(fts, dtype=float), (0, n_feats - len(fts)), constant_values=np.nan
-        )
-
-    return fts[:n_feats]
