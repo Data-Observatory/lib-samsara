9748c161ed67c655dc3938f3a0f4c908ec0aad0b
valegui <valeriaguidotti@gmail.com>
2023-12-26 20:11:42 -0300

    Bump version: 0.1.0 → 0.2.0

--- src/samsara/__init__.py
+++ src/samsara/__init__.py
@@ -4,7 +4,7 @@ SAMSARA: Package for the Satellite Alert and Monitoring System for Areas of Envi
 Using any of these subpackages requires an explicit import. For example,
 ``import samsara.pelt`` or ``from samsara import pelt``.
 """
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     "__version__",
