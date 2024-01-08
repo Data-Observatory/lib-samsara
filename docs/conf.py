3c42c365781eaceed45fe736e059d4e335052986
valegui <valeriaguidotti@gmail.com>
2024-01-08 16:01:46 -0300

    Bump version: 0.2.0 → 0.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ try:
     version = release = get_distribution("samsara").version
 except Exception:
     traceback.print_exc()
-    version = release = "0.2.0"
+    version = release = "0.2.1"
 
 pygments_style = "trac"
 templates_path = ["."]
